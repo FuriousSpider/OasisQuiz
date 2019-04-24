927115cb6ca86d38b0f1d6bf8786af6fc9f06c56
FuriousSpider <mario123gbm@gmail.com>
2019-04-24 11:11:27 +0200

    add realm library

--- app/src/main/java/com/furiousspider/oasisquiz/ui/activities/game/single/quick/QuickSingleGameModel.kt
+++ app/src/main/java/com/furiousspider/oasisquiz/ui/activities/game/single/quick/QuickSingleGameModel.kt
@@ -1,10 +1,11 @@
 package com.furiousspider.oasisquiz.ui.activities.game.single.quick
 
+import com.furiousspider.oasisquiz.utils.QuestionType
 import java.io.File
 
 data class QuickSingleGameModel(
     val id: Int,
-    val questionType: Int,
+    val questionType: QuestionType,
     val question: String,
     val correctAnswer: String,
     val incorrectAnswers: List<String>?,
