520f3e24921e83b8bf405607cc7179b19321d616
FuriousSpider <mario123gbm@gmail.com>
2024-08-06 10:55:30 +0200

    update main menu navigation

--- app/src/main/java/com/oasis/oasisquiz/ui/main_menu/MainMenuFragment.kt
+++ app/src/main/java/com/oasis/oasisquiz/ui/main_menu/MainMenuFragment.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import com.oasis.oasisquiz.R
 import com.oasis.oasisquiz.databinding.FragmentMainMenuBinding
-import com.oasis.oasisquiz.model.Questions
 
 class MainMenuFragment : Fragment() {
 
@@ -35,20 +31,12 @@ class MainMenuFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        testFirebaseAndRemoveLater()
-    }
+        binding.mainMenuQuickGameButton.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenuFragment_to_quickGameFragment)
+        }
 
-    private fun testFirebaseAndRemoveLater() {
-        val db = Firebase.firestore
-
-        db.collection("questions").document("questions").get().addOnSuccessListener { document ->
-            val questions: Questions? = document.toObject(Questions::class.java)
-            questions?.let {
-                Toast.makeText(this@MainMenuFragment.context, "Success: " + it.questions[0].question, Toast.LENGTH_LONG).show()
-            }
-            
-        }.addOnFailureListener { error ->
-            Toast.makeText(this@MainMenuFragment.context, error.message, Toast.LENGTH_LONG).show()
+        binding.mainMenuOptionsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenuFragment_to_optionsFragment)
         }
     }
 }
